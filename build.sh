3c5ba5f0911697d4f3cd2820b54c7a9ffcf70f7b
Daniil Tkachenko <tkachenoko@gmail.com>
2014-12-10 18:47:04 +0300

    Change encoding of emotions dictionary and change build file.

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Вызываем программу управления Cache и передаем ей сформированный скрипт 
-cat credentials.txt build.cos | csession cache
+# Вызываем программу управления Cache и передаем ей сформированный скрипт
+
+mkdir -p /InterSystems/Cache/mgr/IKNOWSOCIAL/Resources
+cp Resources/emotions.txt /InterSystems/Cache/mgr/IKNOWSOCIAL/Resources
+cat credentials.txt <(echo "set jenkinsDir = \"/var/lib/jenkins/workspace/iknowsocial\"") build.cos | csession cache
+
 
 # Если при выполнении скрипта cos был создан файл-флаг ошибки - оповещаем об этом Jenkins
-if [ -f error.flag ]; then
+if [ -f /var/lib/jenkins/workspace/iknowsocial/error.flag ]; then
     echo "Error occured"
-    rm error.flag
+    rm /var/lib/jenkins/workspace/iknowsocial/error.flag
     exit 1
 else
     echo "Success"
     exit 0
 fi
-
